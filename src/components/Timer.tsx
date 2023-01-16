773c79aeac2861565785c94058ee5434a99eb702
catherineair <113204242+catherineair@users.noreply.github.com>
2023-01-16 21:34:19 +0200

    123

--- src/components/Timer.tsx
+++ src/components/Timer.tsx
@@ -1,1 +1,34 @@
+import { type } from "@testing-library/user-event/dist/type";
 import React from "react";
+import timeZones from "../time-zones";
+
+type TimerProps =
+{
+   timeZoneIndex: number;   
+}
+
+export const Timer: React.FC<TimerProps> = (props) =>
+{
+    const timeZone = timeZones[props.timeZoneIndex].name;
+
+    const [time, setTime] = React.useState<Date>(new Date());
+
+    function tick()
+    {
+        console.log("tick");
+        setTime(new Date());   
+    }
+
+    React.useEffect(() =>
+    {
+        const interval = setInterval(tick, 1000);
+        return () => clearInterval(interval);
+    }, []);
+
+    setInterval(tick, 1000);
+
+    return <div>
+        <h3>Time in time zone {timeZone}</h3>
+        <label style={{display: "block", textAlign: "center", fontSize: "2em"}}>Time {time.toLocaleTimeString(undefined, {timeZone})}</label>
+    </div>
+}
