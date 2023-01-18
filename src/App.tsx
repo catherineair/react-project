7faf48ba21f0e0ce8e2ba56b7ede45be1977d6e4
catherineair <113204242+catherineair@users.noreply.github.com>
2023-01-18 13:50:08 +0200

    hmw 33

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App()
   return <div>
     <Timer cityOrCountry={'Canada'} regularity={3}></Timer>
     <Timer cityOrCountry={'Paris'} regularity={6}></Timer>
-    <Timer cityOrCountry={'Island'} regularity={12}></Timer>
+    <Timer cityOrCountry={'Iceland'} regularity={12}></Timer>
     <Timer cityOrCountry={'Milan'} regularity={18}></Timer>
   </div>
 }
